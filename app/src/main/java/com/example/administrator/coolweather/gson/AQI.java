15795b64dfb3d031331919acfa7d3adc2c2c0747
zhangqi <348462636@qq.com>
2017-02-10 17:33:20 +0800

    显示天气到手手机

---
+++ app/src/main/java/com/example/administrator/coolweather/gson/AQI.java
@@ -0,0 +1,19 @@
+package com.example.administrator.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/2/10.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class  AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
